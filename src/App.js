2320ed2a5c543e235e9a55369be4844bc5006e64
Guilherme Cerqueira <gcerqueira@Guilhermes-MacBook-Pro.local>
2023-04-27 12:24:16 -0300

    First commit right

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCWT7nvSTSDQD2zkWM0LgeSKUmhwm7RPJA",
-  authDomain: "cocktail-guide-21a92.firebaseapp.com",
-  projectId: "cocktail-guide-21a92",
-  storageBucket: "cocktail-guide-21a92.appspot.com",
-  messagingSenderId: "54617687243",
-  appId: "1:54617687243:web:03f1ee7f32b0fc1499874e",
-  measurementId: "G-TXQJW0TP84"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
